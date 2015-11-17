bd06ab0356c514f95ed4ebbe712e6a8ef215d602
Stacey Touset <capicue@gmail.com>
2015-11-17 13:17:28 -0800

    Perform the task using a port

--- Main.elm
+++ Main.elm
@@ -1,4 +1,13 @@
 import Graphics.Element exposing (..)
+import ArticleList
+import Api
+import Task exposing (Task)
+import Http
+
+
+port foo : Task Http.RawError Http.Response
+port foo =
+  Api.request Api.Get "articles"
 
 
 main : Element
